e1ca3f4b3ac88c6fcf82d8159c0d6c315a524e2d
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-04-09 20:28:44 +0700

    Refresh Token

--- src/Practical.Jwt.Api/Practical.Jwt.Api/Controllers/UsersController.cs
+++ src/Practical.Jwt.Api/Practical.Jwt.Api/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,6 +18,8 @@ namespace Practical.Jwt.Api.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private static readonly object _lock = new object();
+        private static readonly Dictionary<string, (string Token, DateTimeOffset Expiration)> _refreshTokens = new Dictionary<string, (string Token, DateTimeOffset Expiration)>();
         private readonly IConfiguration _configuration;
 
         public UsersController(IConfiguration configuration)
@@ -34,19 +37,74 @@ namespace Practical.Jwt.Api.Controllers
         [Route("login")]
         public async Task<IActionResult> Login([FromBody] LoginModel model)
         {
+            if (model != null)
+            {
+                model.UserName = model?.UserName?.ToLowerInvariant();
+            }
+
             var authClaims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, "test@abc.com"),
+                new Claim(ClaimTypes.Name, model.UserName),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
                 new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToString()),
             };
 
             var token = CreateToken(authClaims);
+            var refreshToken = GenerateRefreshToken();
+
+            lock (_lock)
+            {
+                _refreshTokens[model.UserName] = (refreshToken, DateTimeOffset.UtcNow.AddHours(24));
+            }
 
             return Ok(new
             {
+                UserName = model.UserName,
                 Token = new JwtSecurityTokenHandler().WriteToken(token),
-                //RefreshToken = refreshToken,
+                RefreshToken = refreshToken,
+                Expiration = token.ValidTo
+            });
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("refreshtoken")]
+        public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenModel model)
+        {
+            if (model != null)
+            {
+                model.UserName = model?.UserName?.ToLowerInvariant();
+            }
+
+            var validRefreshToken = _refreshTokens.ContainsKey(model.UserName)
+                && _refreshTokens[model.UserName].Token == model.RefreshToken
+                && _refreshTokens[model.UserName].Expiration > DateTimeOffset.Now;
+
+            if (!validRefreshToken)
+            {
+                return BadRequest();
+            }
+
+            var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, model.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTimeOffset.UtcNow.ToString()),
+            };
+
+            var token = CreateToken(authClaims);
+            var refreshToken = GenerateRefreshToken();
+
+            lock (_lock)
+            {
+                _refreshTokens[model.UserName] = (refreshToken, DateTimeOffset.UtcNow.AddHours(24));
+            }
+
+            return Ok(new
+            {
+                UserName = model.UserName,
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                RefreshToken = refreshToken,
                 Expiration = token.ValidTo
             });
         }
@@ -63,6 +121,31 @@ namespace Practical.Jwt.Api.Controllers
                 signingCredentials: new SigningCredentials(symmetricKey, SecurityAlgorithms.HmacSha256));
 
             return token;
+        }
+
+        private static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
+        private void ValidateToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidIssuer = _configuration["Jwt:Issuer"],
+                ValidAudience = _configuration["Jwt:Audience"],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SymmetricKey"])),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
         }
     }
 }
--- src/Practical.Jwt.Api/Practical.Jwt.Api/Models/RefreshTokenModel.cs
+++ src/Practical.Jwt.Api/Practical.Jwt.Api/Models/RefreshTokenModel.cs
@@ -1,7 +1,9 @@
 ﻿namespace Practical.Jwt.Api.Models
 {
-    public class LoginModel
+    public class RefreshTokenModel
     {
         public string UserName { get; set; }
+
+        public string RefreshToken { get; set; }
     }
 }
