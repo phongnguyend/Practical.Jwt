e2ca337c966ee4dd9dc70e45a70235e690bd3b67
Phong Nguyen <nguyendoanphong.it@gmail.com>
2022-09-07 16:56:01 +0700

    remove unused code

--- src/Practical.Jwt/Practical.Jwt.Api/Startup.cs
+++ src/Practical.Jwt/Practical.Jwt.Api/Startup.cs
@@ -45,15 +45,6 @@ namespace Practical.Jwt.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseCors(configurePolicy =>
             {
